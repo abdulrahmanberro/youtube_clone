960c163f3d43a7fe957e444b51688eb18cdd6477
Abdulrahman Berro <72647782+abdulrahmanberro@users.noreply.github.com>
2022-09-13 20:45:05 +0300

    structure update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 
+import {
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+  Navbar,
+} from "./components/index";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:id" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;
