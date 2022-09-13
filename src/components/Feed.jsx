fa7ee6b49c45383be18f1b0bf38e261cbe651034
Abdulrahman Berro <72647782+abdulrahmanberro@users.noreply.github.com>
2022-09-13 23:16:49 +0300

    side bar applied (not fully)

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { Sidebar } from "./index";
 
 const Feed = () => {
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          paddingX: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          calssName="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          &copy; Copyright 2022 YT Clone Made By Abdulrahman
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as VideoDetail } from "./VideoDetail";
 export { default as SearchFeed } from "./SearchFeed";
 export { default as Navbar } from "./Navbar";
 export { default as SearchBar } from "./SearchBar";
+export { default as Sidebar } from "./Sidebar";
