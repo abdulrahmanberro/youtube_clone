6d2dfd8ff0b0c2c5385f9376f8f66bebd841dc3e
Abdulrahman Berro <72647782+abdulrahmanberro@users.noreply.github.com>
2022-09-13 21:43:58 +0300

    update navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-
-
+import React from "react";
+import Stack from "@mui/material/Stack";
+import { Link } from "react-router-dom";
+import { SearchBar } from "./index";
+import { logo } from "../utils/constants";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        background: "#000",
+        top: 0,
+        justifyContent: "space-between",
+      }}
+    >
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
 
-export default Navbar
+      <SearchBar />
+    </Stack>
+  );
+};
+
+export default Navbar;
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Feed } from "./Feed";
 export { default as VideoDetail } from "./VideoDetail";
 export { default as SearchFeed } from "./SearchFeed";
 export { default as Navbar } from "./Navbar";
+export { default as SearchBar } from "./SearchBar";
